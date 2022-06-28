a782cc6e6761301a02b8bbe6625081f33df60c6f
Ronalds <ronaldsronix@gmail.com>
2022-06-28 15:52:21 +0300

    fifth excercise upload

--- fifth.java
+++ fifth.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class fifth {
+    public static void main(String[] args) {
+        System.out.println("Enter boolean value:");
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        if(x.equals("True") || x.equals("true")){
+            System.out.println("the opposite of \"True\" is \"False\"");
+        } else if (x.equals("False") || x.equals("false")){
+            System.out.println("the opposite of \"False\" is \"True\"");
+        }
+    }
 }
