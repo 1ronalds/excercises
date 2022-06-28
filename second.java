68a658babd53367ed8072913905bf32c509dfbb2
Ronalds <ronaldsronix@gmail.com>
2022-06-28 15:57:16 +0300

    fix

--- second.java
+++ second.java
@@ -7,9 +7,9 @@ public class second {
         System.out.println("Enter b2: ");
         String b = sc.nextLine();
 
-        if(a == b && (a == "True" || a == "true" || a == "false" || a == "False") && (b == "True" || b == "true" || b == "false" || b =="False")) {
+        if(a.equals(b) && (a.equals("True") || a.equals("true") || a.equals("false") || a.equals("False")) && (b.equals("True") || b.equals("true") || b.equals("false") || b.equals("False"))) {
             System.out.println("Is b1 and b2 equal? - true");
-        } else if (a != b && (a == "True" | a == "true" | a == "false" | a == "False") && (b == "True" | b == "true" | b == "false" | b =="False")){
+        } else if (!a.equals(b) && (a.equals("True") || a.equals("true") || a.equals("false") || a.equals("False")) && (b.equals("True") || b.equals("true") || b.equals("false") || b.equals("False"))){
             System.out.println("Is b1 and b2 equal? - false");
         }
 
