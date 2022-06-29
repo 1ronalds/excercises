c30a5699319456dda8371fdf81e12f4139299bac
Ronalds <ronaldsronix@gmail.com>
2022-06-29 11:32:15 +0300

    second exercise of day 3

---
+++ day3_second.java
@@ -0,0 +1,14 @@
+public class day3_second {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        int arrb[] = new int[4];
+        arr[0] = 2;
+        arr[1] = 4;
+        arr[2] = 6;
+        arr[3] = 8;
+
+        for(int i = 0; i < arr.length; i++){
+            arrb[i] = arr[i];
+        }
+    }
+}
