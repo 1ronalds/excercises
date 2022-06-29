5fa3c1a169ec62c52f7633903e13d49950878213
Ronalds <ronaldsronix@gmail.com>
2022-06-29 11:23:02 +0300

    first exercise of day 3

---
+++ day3_first.java
@@ -0,0 +1,15 @@
+public class day3_first {
+    public static void main(String[] args){
+        int[][] table = new int[10][10];
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+                table[i][j] = (i+1) * (j+1);
+            }
+        }
+        for(int i=0; i < 10; i++){
+            for(int j=0; j < 10; j++){
+                System.out.println((i+1) + " * " + (j+1) + " = " + table[i][j]);
+            }
+        }
+    }
+}
