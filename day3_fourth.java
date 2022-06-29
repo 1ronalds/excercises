8131fbcc0f66cbef44eea4a2f7cbab79d2056e55
Ronalds <ronaldsronix@gmail.com>
2022-06-29 11:47:55 +0300

    fix

--- day3_fourth.java
+++ day3_fourth.java
@@ -1,24 +1,27 @@
 class BankAccount {
-    public double balance;
-    public BankAccount(double balance){
+    private double balance;
+    public void BankAccount(double balance){
         this.balance = balance;
     }
     public void deposit(double amount){
         this.balance += amount;
     }
     public void withdraw(double amount){
-        this.balance -= amount;
+        if(this.balance - amount >= 0) {
+            this.balance -= amount;
+        } else {
+            System.out.println("There is not enough funds.");
+        }
     }
     public void printBalance(){
         System.out.println(this.balance);
     }
-    public double transfer(double amount){
-        this.balance -= amount;
-        return amount;
+    public double transfer(){
+        return this.balance;
     }
 }
 
-public class day3_third {
+public class day3_fourth {
     public static void main(String[] args){
 
     }
